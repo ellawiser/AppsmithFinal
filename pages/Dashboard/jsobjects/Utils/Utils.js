c774dc97c827e3cf4b41de32b701af995efd0cb0
Nathalie Abello-Sudarsky <nathalieabello1@gmail.com>
2023-12-02 21:47:18 +0000

    utils for sleep

--- pages/Dashboard/jsobjects/Utils/Utils.js
+++ pages/Dashboard/jsobjects/Utils/Utils.js
@@ -1,39 +1,45 @@
 export default {
-	formatDate: function(dateString) {
-    const dateObject = new Date(dateString);
-    return dateObject.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' });
-  },
+	formatDate: function (dateString) {
+		const dateObject = new Date(dateString);
+		return dateObject.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' });
+	},
 
-  transformStepData: () => {
-    return getSteps.data.map(item => ({
-      "x": this.formatDate(item.date),
-      "y": item.stepCount
-    }));
-  },
-	
-	sleepData: [
-    {
-      "NREMTime": 12,
-      "REMTime": 3,
-      "datetimeEnded": "Fri, 10 Nov 2023 10:15:00 GMT",
-      "datetimeStarted": "Thu, 09 Nov 2023 21:45:00 GMT",
-      "id": 3
-    },
-    {
-      "NREMTime": 12,
-      "REMTime": 3,
-      "datetimeEnded": "Sat, 11 Nov 2023 11:15:00 GMT",
-      "datetimeStarted": "Fri, 10 Nov 2023 21:45:00 GMT",
-      "id": 4
-    }
-  ],
-	
-	transformSleepData: () => {
-    return getSleep.data.map(item => ({
-      "x": getSleep.data(item.datetimeStarted),
-      "y": item.REMTime
-    }));
-  },
-	
-	
+	transformStepData: function () {
+		return getSteps.data.map(item => ({
+			"x": this.formatDate(item.date),
+			"y": item.stepCount
+		}));
+	},
+
+	transformSleepData: function () {
+		return getSleep.data.map(item => ({
+			"x": this.formatDate(item.datetimeStarted),
+			"y": item.REMTime
+		}));
+	},
+
+	REMcolor: function (percentageDifference) {
+		const grey = "#333333";
+		const red = "#FF0000";
+		const green = "#00FF00";
+
+		if (percentageDifference > 0) {
+			return green;
+		} else if (percentageDifference === 0) {
+			return grey;
+		} else {
+			return red;
+		}
+	},
+
+	REMmsg: function (percentageDifference) {
+		// compare rem times
+		if (percentageDifference > 0) {
+			console.log("Your most recent sleep was better than average by " + percentageDifference + "%!");
+		} else if (percentageDifference < 0) {
+			console.log("Your most recent sleep was worse than average by " + percentageDifference + "%");
+		} else {
+			console.log("Your most recent sleep was the same as your average.");
+		}
+	}
 };
