965f4618f2641edcbc69eeb0d417152cecf7e994
Nathalie Abello-Sudarsky <nathalieabello1@gmail.com>
2023-12-02 20:38:32 +0000

    utils

--- pages/Dashboard/jsobjects/Utils/Utils.js
+++ pages/Dashboard/jsobjects/Utils/Utils.js
@@ -1,26 +1,39 @@
 export default {
-	  originalData: [
-    {
-      "date": "Tue, 26 Nov 2019 00:00:00 GMT",
-      "id": 8,
-      "stepCount": 23425
-    },
-    {
-      "date": "Mon, 17 Apr 2023 00:00:00 GMT",
-      "id": 3,
-      "stepCount": 13000
-    }
-  ],
-	
-  formatDate: (dateString) => {
+	formatDate: function(dateString) {
     const dateObject = new Date(dateString);
-    return dateObject.toLocaleDateString('en-US', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' });
+    return dateObject.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' });
   },
 
-  transformData: () => {
-    return this.originalData.map(item => ({
+  transformStepData: () => {
+    return getSteps.data.map(item => ({
       "x": this.formatDate(item.date),
       "y": item.stepCount
     }));
-  }
+  },
+	
+	sleepData: [
+    {
+      "NREMTime": 12,
+      "REMTime": 3,
+      "datetimeEnded": "Fri, 10 Nov 2023 10:15:00 GMT",
+      "datetimeStarted": "Thu, 09 Nov 2023 21:45:00 GMT",
+      "id": 3
+    },
+    {
+      "NREMTime": 12,
+      "REMTime": 3,
+      "datetimeEnded": "Sat, 11 Nov 2023 11:15:00 GMT",
+      "datetimeStarted": "Fri, 10 Nov 2023 21:45:00 GMT",
+      "id": 4
+    }
+  ],
+	
+	transformSleepData: () => {
+    return getSleep.data.map(item => ({
+      "x": getSleep.data(item.datetimeStarted),
+      "y": item.REMTime
+    }));
+  },
+	
+	
 };
