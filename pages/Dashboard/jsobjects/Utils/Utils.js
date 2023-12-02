e7698b13d961f32730dfad2e2d97b9d23da296b0
Nathalie Abello-Sudarsky <nathalieabello1@gmail.com>
2023-12-02 21:56:21 +0000

    fixed utils

--- pages/Dashboard/jsobjects/Utils/Utils.js
+++ pages/Dashboard/jsobjects/Utils/Utils.js
@@ -17,27 +17,37 @@ export default {
 			"y": item.REMTime
 		}));
 	},
+	
+	compareREMTime: function () {
+		const sleepData = getSleep.data.sort((a, b) => new Date(b.datetimeStarted) - new Date(a.datetimeStarted));
+		const mostRecentREMTime = sleepData[0].REMTime;
+		const averageREMTime = sleepData.reduce((total, entry) => total + entry.REMTime, 0) / sleepData.length;
+		const percentageDifference = ((mostRecentREMTime - averageREMTime) / averageREMTime) * 100;
+		return percentageDifference;
+	},
 
-	REMcolor: function (percentageDifference) {
+	REMcolor: function () {
+		const percent = this.compareREMTime();
 		const grey = "#333333";
 		const red = "#FF0000";
 		const green = "#00FF00";
 
-		if (percentageDifference > 0) {
+		if (percent > 0) {
 			return green;
-		} else if (percentageDifference === 0) {
+		} else if (percent === 0) {
 			return grey;
 		} else {
 			return red;
 		}
 	},
 
-	REMmsg: function (percentageDifference) {
+	REMmsg: function () {
+		const percent = this.compareREMTime();
 		// compare rem times
-		if (percentageDifference > 0) {
-			console.log("Your most recent sleep was better than average by " + percentageDifference + "%!");
-		} else if (percentageDifference < 0) {
-			console.log("Your most recent sleep was worse than average by " + percentageDifference + "%");
+		if (percent > 0) {
+			console.log("Your most recent sleep was better than average by " + percent + "%!");
+		} else if (percent < 0) {
+			console.log("Your most recent sleep was worse than average by " + percent + "%");
 		} else {
 			console.log("Your most recent sleep was the same as your average.");
 		}
