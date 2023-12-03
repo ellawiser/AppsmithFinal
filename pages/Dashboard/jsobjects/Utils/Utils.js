c99911b07726f222ef48b2f4be6dbf606099c530
Nathalie Abello-Sudarsky <nathalieabello1@gmail.com>
2023-12-03 00:39:31 +0000

    all implementations for sign up, log in, and dashboard

--- pages/Dashboard/jsobjects/Utils/Utils.js
+++ pages/Dashboard/jsobjects/Utils/Utils.js
@@ -1,32 +1,41 @@
 export default {
-	formatDate: function (dateString) {
-		const dateObject = new Date(dateString);
-		return dateObject.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' });
-	},
-	
-	formatDateForDB: function (dateString) {
-		const dateObject = new Date(dateString);
-		const year = dateObject.getUTCFullYear();
-		const month = (dateObject.getUTCMonth() + 1).toString().padStart(2, '0');
-		const day = dateObject.getUTCDate().toString().padStart(2, '0');
-		const hours = dateObject.getUTCHours().toString().padStart(2, '0');
-		const minutes = dateObject.getUTCMinutes().toString().padStart(2, '0');
-		const seconds = dateObject.getUTCSeconds().toString().padStart(2, '0');
-		return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-	},
-
 	transformStepData: function () {
 		return getSteps.data.map(item => ({
-			"x": this.formatDate(item.date),
+			"x": dateUtils.formatDate(item.date),
 			"y": item.stepCount
 		}));
 	},
 
 	transformSleepData: function () {
 		return getSleep.data.map(item => ({
-			"x": this.formatDate(item.datetimeStarted),
+			"x": dateUtils.formatDate(item.datetimeStarted),
 			"y": item.REMTime
 		}));
+	},
+
+	transformMacrosData: function () {
+		const data = [];
+		const calories = getMacros.data.map(item => ({
+			"x": dateUtils.formatDate(item.date),
+			"y": item.calorieCount
+		}));
+		data.push(calories);
+		const protein = getMacros.data.map(item => ({
+			"x": dateUtils.formatDate(item.date),
+			"y": item.proteinCount
+		}));
+		data.push(protein);
+		const carbs = getMacros.data.map(item => ({
+			"x": dateUtils.formatDate(item.date),
+			"y": item.carbCount
+		}));
+		data.push(carbs);
+		const fats = getMacros.data.map(item => ({
+			"x": dateUtils.formatDate(item.date),
+			"y": item.fatCount
+		}));
+		data.push(fats);
+		return data;
 	}
 
 };
